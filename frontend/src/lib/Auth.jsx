b4b65cd22a2bbb2c3d81c7946e01d8bbac878132
aminebenfraj <mohamedamine.benfredj@polytechnicien.tn>
2025-07-14 10:56:46 +0200

    feat: update API URLs to use production endpoint for backend services

--- frontend/src/lib/Auth.jsx
+++ frontend/src/lib/Auth.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api/auth"
+const API_URL = "https://novares-sharepoint-backend.onrender.comapi/auth"
 
 // Get current user
 export const getCurrentUser = async () => {
