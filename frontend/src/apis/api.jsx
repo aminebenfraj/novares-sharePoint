d1ff733f44b0bb380145007c6f3f0243cec8b424
aminebenfraj <mohamedamine.benfredj@polytechnicien.tn>
2025-07-22 10:49:48 +0200

    api deployment change

--- frontend/src/apis/api.jsx
+++ frontend/src/apis/api.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:5000/`,
+  baseURL: `https://novares-sharepoint-backend.onrender.com/`,
   timeout: 5000,
 });
 
